918d2b395500718e8f8e878a9f929ef117a0d19d
Antoine L <162304112+t0rbenC0rtes@users.noreply.github.com>
2025-04-07 11:36:18 +0200

    Update Map component to use new Google Maps embed URL, fridge view

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,15 +2,15 @@ const Map = () => {
 	return (
 		<div style={{ width: "100%", height: "500px" }}>
 			<iframe
-				src="https://www.google.com/maps/embed?pb=!4v1744017291660!6m8!1m7!1sCAoSF0NJSE0wb2dLRUlDQWdJQ1JpN1NpM0FF!2m2!1d50.85571090112251!2d4.389122572015062!3f61.44506279816306!4f1.7736447667740975!5f0.7820865974627469"
+				src="https://www.google.com/maps/embed?pb=!4v1744018531055!6m8!1m7!1sCAoSFkNJSE0wb2dLRUlDQWdJQ1JpN1NpYUE.!2m2!1d50.85573930288061!2d4.389145744067452!3f52.28713635116376!4f-47.30770901512865!5f0.4000000000000002"
 				width="100%"
 				height="450px"
 				style={{ border: 0 }}
 				allowFullScreen=""
 				loading="lazy"
 				referrerPolicy="no-referrer-when-downgrade"
-                title="Google Street View"
-			></iframe>
+				title="Google Street View"
+			></iframe>			
 		</div>
 	);
 };
