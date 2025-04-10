30f8dbf9d5ebf76dcd9a045fc038276a7e564daf
Antoine L <162304112+t0rbenC0rtes@users.noreply.github.com>
2025-04-10 13:57:15 +0200

    Refactor Navbar component to see lang options on refresh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,45 +2,46 @@ import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 const Navbar = ({ setLanguage }) => {
-  const [showNavbar, setShowNavbar] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
+	const [showNavbar, setShowNavbar] = useState(true);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
+	useEffect(() => {
+		let lastScrollY = window.scrollY;
 
-      if (currentScrollY > lastScrollY) {
-        setShowNavbar(false);
-      } else {
-        setShowNavbar(true);
-      }
+		const handleScroll = () => {
+			const currentScrollY = window.scrollY;
 
-      setLastScrollY(currentScrollY);
-    };
+			if (currentScrollY > lastScrollY) {
+				setShowNavbar(false);
+			} else {
+				setShowNavbar(true);
+			}
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollY]);
+			lastScrollY = currentScrollY;
+		};
 
-  return (
-    <motion.div
-      className="navbar"
-      initial={{ y: 0 }}
-      animate={{ y: showNavbar ? 0 : -100 }}
-      transition={{ duration: 0.3 }}
-      style={{
-        position: "fixed",
-        top: 10,
-        right: 10,
-        zIndex: 1000,
-      }}
-    >
-      <button onClick={() => setLanguage("nl")}>NL</button>
-      <button onClick={() => setLanguage("fr")}>FR</button>
-      <button onClick={() => setLanguage("en")}>EN</button>
-      <button onClick={() => setLanguage("es")}>ES</button>
-    </motion.div>
-  );
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
+	return (
+		<motion.div
+			className="navbar"
+			initial={{ y: 0 }}
+			animate={{ y: showNavbar ? 0 : -100 }}
+			transition={{ duration: 0.3 }}
+			style={{
+				position: "fixed",
+				top: 10,
+				right: 10,
+				zIndex: 1000,
+			}}
+		>
+			<button onClick={() => setLanguage("nl")}>NL</button>
+			<button onClick={() => setLanguage("fr")}>FR</button>
+			<button onClick={() => setLanguage("en")}>EN</button>
+			<button onClick={() => setLanguage("es")}>ES</button>
+		</motion.div>
+	);
 };
 
 export default Navbar;
